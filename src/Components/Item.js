521d68ca09cfa716d52bc170d97cf5a0fd537c19
GeorgeBrovko <114675142+GeorgeBrovko@users.noreply.github.com>
2023-07-12 20:32:00 +0700

    first commit

---
+++ src/Components/Item.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export class Item extends Component {
+    render() {
+        return (
+            <div className='item'>
+                <img src={"./img/" + this.props.item.img} alt="My" />
+                <h2>{this.props.item.title}</h2>
+                <h3>Вес: {this.props.item.Ves} г</h3>
+                <p>{this.props.item.desc}</p>
+                <b>{this.props.item.price}р </b>
+                <div className="add-to-cart" onClick={() => this.props.onAdd(this.props.item)}>В корзину</div>
+            </div>
+        )
+    }
+}
+
+export default Item
